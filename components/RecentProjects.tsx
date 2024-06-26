fb9c0bb58d665336e07847e344304bd6be0091ca
Zamil <zamilaziz92@gmail.com>
2024-06-26 13:18:38 +0800

    Complete project

---
+++ components/RecentProjects.tsx
@@ -0,0 +1,108 @@
+import { projects } from "@/data";
+import { PinContainer } from "./ui/ThreeDPin";
+import { FaLocationArrow } from "react-icons/fa6";
+
+const RecentProjects = () => {
+    return(
+        <div className="py-20">
+            <h1 className="heading">
+                A selection of{' '}
+                <span className="text-purple">recent projects</span>
+            </h1>
+            <div
+                id="projects"
+                className="
+                    flex 
+                    flex-wrap 
+                    items-center 
+                    justify-center 
+                    p-4 
+                    gap-x-24
+                    gap-y-8 
+                    mt-10
+                    "
+                >
+                {projects.map(({id, title, des, img, link, iconLists}) => (
+                    <div 
+                        key={id}
+                        className="
+                            sm:h-[41rem]
+                            lg:min-h-[32.5rem]
+                            h-[32rem]
+                            flex
+                            items-center
+                            justify-center
+                            sm:w-[570px]
+                            w-[80vw]
+                        "
+                    >   
+                        <PinContainer title={title} href={link}>
+                            <div 
+                                className="
+                                    relative 
+                                    flex 
+                                    items-center 
+                                    justify-center 
+                                    sm:w-[570px]
+                                    sm:h-[40vh]
+                                    w-[80vw] 
+                                    overflow-hidden 
+                                    h-[30vh]
+                                    lg:h-[30vh]
+                                    mb-10
+                                    "
+                                >
+                                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
+                                    <img src="bg.png" alt="bg-img"/>
+                                </div>
+                                <img
+                                    src={img}
+                                    alt={title}
+                                    className="z-10 absolute right-0"
+                                />
+                            </div>
+                            <h1 className="pb-4 font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                                {title}
+                            </h1>
+                            <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-8">
+                                {des}
+                            </p>
+                            <div className="flex items-center justify-between mt-7 mb-3">
+                                <div className="flex items-center">
+                                    {iconLists.map((icon,index) => (
+                                        <div 
+                                            key={icon}
+                                            style={{
+                                                transform: `translateX(-${5 * index * 2}px)`
+                                            }}
+                                            className="
+                                                border 
+                                                border-white/[0.2] 
+                                                rounded-full
+                                                bg-black
+                                                lg:w-10
+                                                lg:h-10
+                                                w-10
+                                                h-10
+                                                flex
+                                                justify-center
+                                                items-center
+                                                "
+                                            >
+                                            <img src={icon} alt={icon} className="p-2"/>
+                                        </div>
+                                    ))}
+                                </div>
+                                <div className="flex justify-center items-center">
+                                    <p className="flex lg:text-xl md:text-xs text-sm text-purple">Check Live Site</p>
+                                    <FaLocationArrow className="ms-3" color="#CBACF9"/>
+                                </div>
+                            </div>
+                        </PinContainer>     
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+export default RecentProjects;
---
+++ data/index.tsx
@@ -0,0 +1,105 @@
+import { FaStrava } from "react-icons/fa6";
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Client collaboration and open communication are my top priorities",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh] text-dark",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/client.png",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I am highly flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I consistently seek ways to enhance my abilities",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Enthusiastic about technology and web development",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: (
+        <span>
+          Converting your{' '}
+          <span style={{ color: '#00ff9b' }}>ideas</span> into reality through a blend of creativity and effective execution
+        </span>
+      ),
+      description: "You say it, I build it",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute w-full h-full",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/code.jpg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Milbnb: A Fullstack Airbnb Replicate",
+      des: "Milbnb is a comprehensive fullstack web application inspired by Airbnb, designed to provide users with a seamless experience for booking and listing accommodations. Leveraging modern technologies, Milbnb offers an intuitive interface for users to browse, search, and reserve properties, while enabling hosts to list their spaces, manage bookings, and interact with guests. Built with robust backend infrastructure and a responsive frontend, Milbnb ensures reliability, scalability, and a smooth user experience across all devices.",
+      img: "/p1.png",
+      iconLists: ["next.svg", "/re.svg",  "/tail.svg", "/ts.svg", "/prisma.png","/mongo.svg"],
+      link: "https://milbnb.vercel.app/",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link:"https://github.com/timunlipat",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      link:"https://x.com/zamilaziz2",
+    },
+    {
+      id: 3,
+      img: "/strava.svg",
+      link:"https://www.strava.com/athletes/112389118"
+      ,
+    },
+  ];
